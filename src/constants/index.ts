4bf8f5a2d838b109517312e452bc50403422444c
VickyJay <victoluolatunji@gmail.com>
2024-06-26 04:38:12 +0100

    add react helmet stuffs for page title and seo shits

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const META_TITLE: string = "Walletbend";
+export const META_DESCRIPTION: string =
+  "Connect your two or more bank account together and pay for transaction on one app.";
+export const META_IMAGE: string = "";
+
+export const META_URL: string = "https://walletbend.vercel.app/"; //will change to  the real domain later
+export const META_TYPE: string = "website";
+export const META_AUTHOR: string = "Walletbend Dev Team";
+export const META_PUBLISHER: string = "Walletbend Inc";
+export const META_KEYWORDS: string =
+  "Walletbend, Camera, Photography, Videos, Audio, Creativity, design";
+
+// api url
+export const BASE_API_URL = "";
