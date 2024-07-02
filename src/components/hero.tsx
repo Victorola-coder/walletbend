5b995a6b0e32c7ed07bf6a860ba930514d55a746
VickyJay <victoluolatunji@gmail.com>
2024-07-02 02:46:11 +0100

    lib

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <section className="flex flex-col items-center mt-10 mx-[24px]_">
-      <div className="max-w-[342px]">
-        <h4 className="max-w-max font-kai font-medium text-[32px] leading-[41.6px] text-center text-[#000F40]">
+      <div className="max-w-[342px] md:max-w-[812px]">
+        <h4 className="max-w-max font-kai font-medium text-[32px] leading-[41.6px] text-center text-[#000F40] md:text-[52px] md:leading-[62.4px]">
           The Financial Companion for the new generation
         </h4>
-        <p className="font-normal text-base leading-[24px] text-center text-[#80879F]">
+        <p className="font-normal text-base leading-[24px] text-center text-[#80879F] md:text-[18px] md:leading-[27px]">
           Your comprehensive financial companion, providing a unified view of
           your finances in a single, user-friendly interface. Connecting all
           your bank accounts, credit cards.
@@ -15,13 +15,22 @@ export default function Hero() {
         Get Started Now
       </button>
 
-      <figure className="mt-[28px]">
+      <figure className="block md:hidden mt-[28px] mx-0 md:mt-[64px] md:mx-[120px]">
         <img
           src="/images/hero.png"
           alt="hero"
           draggable={false}
           loading="lazy"
           className="transition-all duration-300 hover:scale-[0.98]"
+        />
+      </figure>
+      <figure className="md:block hidden mt-[28px] mx-0 md:mt-[64px] md:mx-[120px]">
+        <img
+          src="/images/heroo.png"
+          alt="hero_dewktop"
+          draggable={false}
+          loading="lazy"
+          className="transition-all duration-300 hover:scale-[1]"
         />
       </figure>
     </section>
--- src/components/partner.tsx
+++ src/components/partner.tsx
@@ -1,4 +1,4 @@
-import { Binance, Bitcoin, Bitmex, Tether } from "./svgs";
+import { Binance, Coinbase, Bitcoin, Bitmex, Tether } from "./svgs";
 
 export default function Partner() {
   return (
@@ -7,13 +7,14 @@ export default function Partner() {
         <h4 className="block mx-auto font-kai font-medium text-[32px] leading-[41.6px] text-center text-[#000F40]">
           Our Partners
         </h4>
-        <p className="font-normal text-base leading-[24px] text-center text-[#80879F]">
+        <p className="font-normal text-base leading-[24px] text-center text-[#80879F] md:text-[18px]">
           Some of our top finance platforms partners.
         </p>
       </div>
 
-      <div className="animate-alternate_  flex flex-row items-center overflow-auto gap-[70px] py-[30px] animate-fade-in">
+      <div className="animate-alternate_  flex flex-row  flex-wrap flex-1 flex-auto items-center overflow-auto gap-[70px] py-[30px] animate-fade-in">
         <Binance />
+        <Coinbase />
         <Bitcoin />
         <Tether />
         <Bitmex />
