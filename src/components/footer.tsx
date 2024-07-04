cfa854462e177a47d202330d396553afc9c6c545
VickyJay <victoluolatunji@gmail.com>
2024-07-05 00:52:05 +0100

    .

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -25,37 +25,35 @@ export default function Footer() {
   return (
     <footer className="bg-[#2E5EFF] px-[24px] py-[32px] flex flex-col md:flex  md:flex-row md:items-center">
       <div>
-        <div>
-          <figure>
-            <img
-              src="/images/qr.png"
-              alt="walletbend.app"
-              draggable={false}
-              loading="lazy"
-            />
-          </figure>
-
-          <p className="font-normal max-w-[342px] font-kai text-[24px] leading-[34.75px] text-left text-white">
-            Get control of your finance now
-          </p>
-          {/* app and google dowwnload */}
-          <div className="flex flex-row items-center justify-center mt-[24px] gap-[28.45px]">
-            <AppleStore />
-            <PlayStore />
-          </div>
-        </div>
-
-        {/* image */}
-        <figure className="my-[32px]">
+        <figure>
           <img
-            src="/images/app.png"
-            alt="walletbend_app_screenshot"
+            src="/images/qr.png"
+            alt="walletbend.app"
             draggable={false}
             loading="lazy"
-            className="object-cover"
           />
         </figure>
+
+        <p className="font-normal max-w-[342px] font-kai text-[24px] leading-[34.75px] text-left text-white">
+          Get control of your finance now
+        </p>
+        {/* app and google dowwnload */}
+        <div className="flex flex-row items-center justify-center mt-[24px] gap-[28.45px]">
+          <AppleStore />
+          <PlayStore />
+        </div>
       </div>
+
+      {/* image */}
+      <figure className="my-[32px]">
+        <img
+          src="/images/app.png"
+          alt="walletbend_app_screenshot"
+          draggable={false}
+          loading="lazy"
+          className="object-cover"
+        />
+      </figure>
 
       {/* line */}
       <div className="bg-white h-[1px]"></div>
